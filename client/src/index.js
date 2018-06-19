737835e201a0511ea34de9e3bf7de5d61c9d9323
Gary Shurgin <master_vorlin@yahoo.com>
2018-06-18 23:45:30 -0400

    Heroku change

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import './css/main.css'
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/index';
-import Header from './header/Header';
-import Body from './Body';
+import store from './js/store/index';
+import Header from './js/header/Header';
+import Body from './js/Body';
 
 render( 
     <Provider store={ store }>
