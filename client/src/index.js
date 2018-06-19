5fa30fee6a1f63cbfc7d9ad91657e7a4d399cb21
Gary Shurgin <master_vorlin@yahoo.com>
2018-06-18 22:16:12 -0400

    Heroku change

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,18 @@
-import index from './js/Main'
 import './css/main.css'
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import Header from './header/Header';
+import Body from './Body';
+
+render( 
+    <Provider store={ store }>
+        <div>
+            <Header />
+            <Body />
+        </div>
+    </Provider>,
+    document.getElementById('app')
+);
