c733337b1a50838b60eb7a147f98ae3d8d1c7e96
Gary Shurgin <master_vorlin@yahoo.com>
2018-06-18 22:01:52 -0400

    Heroku change

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import express from 'express'
 
 var app = express()
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/client/src/'))
 app.listen(process.env.PORT || 3030)
